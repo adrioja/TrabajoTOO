1b653e785a2fe91d6838e79c6f164f07298f89c1
adrioja <adrioja@unirija.es>
2020-12-09 17:36:02 +0100

    Inicio y tabla vehiculo

---
+++ TrabajoTOO/VehiculoTabla.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace TrabajoTOO
+{
+    class VehiculoTabla : KeyedCollection<String, VehiculoDatos>
+    {
+        protected override string GetKeyForItem(VehiculoDatos item)
+        {
+            return item.NumBastidor;
+        }
+    }
+}
---
+++ Vehiculo/Vehiculo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehiculo
+{
+    public class Vehiculo
+    {
+        public Vehiculo()
+        {
+
+        }
+    }
+}
