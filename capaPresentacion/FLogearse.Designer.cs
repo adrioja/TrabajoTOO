538779d979d322d26a90f3f01f12a5c7d9183c95
adrioja <adrioja@unirija.es>
2020-12-21 11:48:39 +0100

    capa presentacion

---
+++ capaPresentacion/FLogearse.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace capaPresentacion
+{
+    partial class FLogearse
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FLogearse));
+            this.tbUsuario = new System.Windows.Forms.TextBox();
+            this.tbContraseña = new System.Windows.Forms.TextBox();
+            this.lUsuario = new System.Windows.Forms.Label();
+            this.lContraseña = new System.Windows.Forms.Label();
+            this.btEntrar = new System.Windows.Forms.Button();
+            this.lDescripcion = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbUsuario
+            // 
+            resources.ApplyResources(this.tbUsuario, "tbUsuario");
+            this.tbUsuario.Name = "tbUsuario";
+            // 
+            // tbContraseña
+            // 
+            resources.ApplyResources(this.tbContraseña, "tbContraseña");
+            this.tbContraseña.Name = "tbContraseña";
+            // 
+            // lUsuario
+            // 
+            resources.ApplyResources(this.lUsuario, "lUsuario");
+            this.lUsuario.Name = "lUsuario";
+            // 
+            // lContraseña
+            // 
+            resources.ApplyResources(this.lContraseña, "lContraseña");
+            this.lContraseña.Name = "lContraseña";
+            // 
+            // btEntrar
+            // 
+            resources.ApplyResources(this.btEntrar, "btEntrar");
+            this.btEntrar.Name = "btEntrar";
+            this.btEntrar.UseVisualStyleBackColor = true;
+            // 
+            // lDescripcion
+            // 
+            resources.ApplyResources(this.lDescripcion, "lDescripcion");
+            this.lDescripcion.Name = "lDescripcion";
+            // 
+            // FLogearse
+            // 
+            this.AcceptButton = this.btEntrar;
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lDescripcion);
+            this.Controls.Add(this.btEntrar);
+            this.Controls.Add(this.lContraseña);
+            this.Controls.Add(this.lUsuario);
+            this.Controls.Add(this.tbContraseña);
+            this.Controls.Add(this.tbUsuario);
+            this.MaximizeBox = false;
+            this.Name = "FLogearse";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbUsuario;
+        private System.Windows.Forms.TextBox tbContraseña;
+        private System.Windows.Forms.Label lUsuario;
+        private System.Windows.Forms.Label lContraseña;
+        private System.Windows.Forms.Button btEntrar;
+        private System.Windows.Forms.Label lDescripcion;
+    }
+}
+
---
+++ capaPresentacion/FPrincipal.Designer.cs
@@ -0,0 +1,94 @@
+﻿
+namespace capaPresentacion
+{
+    partial class FPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tsmiVehiculo = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiCliente = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiPresupuesto = new System.Windows.Forms.ToolStripMenuItem();
+            this.msPrincipal = new System.Windows.Forms.MenuStrip();
+            this.msPrincipal.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tsmiVehiculo
+            // 
+            this.tsmiVehiculo.Name = "tsmiVehiculo";
+            this.tsmiVehiculo.Size = new System.Drawing.Size(77, 19);
+            this.tsmiVehiculo.Text = "Vehiculo";
+            // 
+            // tsmiCliente
+            // 
+            this.tsmiCliente.Name = "tsmiCliente";
+            this.tsmiCliente.Size = new System.Drawing.Size(77, 19);
+            this.tsmiCliente.Text = "Cliente";
+            // 
+            // tsmiPresupuesto
+            // 
+            this.tsmiPresupuesto.Name = "tsmiPresupuesto";
+            this.tsmiPresupuesto.Size = new System.Drawing.Size(77, 19);
+            this.tsmiPresupuesto.Text = "Presupuesto";
+            // 
+            // msPrincipal
+            // 
+            this.msPrincipal.Dock = System.Windows.Forms.DockStyle.Left;
+            this.msPrincipal.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiVehiculo,
+            this.tsmiCliente,
+            this.tsmiPresupuesto});
+            this.msPrincipal.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.VerticalStackWithOverflow;
+            this.msPrincipal.Location = new System.Drawing.Point(0, 0);
+            this.msPrincipal.Name = "msPrincipal";
+            this.msPrincipal.Size = new System.Drawing.Size(90, 395);
+            this.msPrincipal.TabIndex = 0;
+            this.msPrincipal.Text = "menuStrip1";
+            // 
+            // FPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 395);
+            this.Controls.Add(this.msPrincipal);
+            this.MainMenuStrip = this.msPrincipal;
+            this.Name = "FPrincipal";
+            this.Text = "Concesionario";
+            this.msPrincipal.ResumeLayout(false);
+            this.msPrincipal.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStripMenuItem tsmiVehiculo;
+        private System.Windows.Forms.ToolStripMenuItem tsmiCliente;
+        private System.Windows.Forms.ToolStripMenuItem tsmiPresupuesto;
+        private System.Windows.Forms.MenuStrip msPrincipal;
+    }
+}
